981c1935209ee68e91970f9e24ed4922b0fe3e23
RaPGoD31 <ardakaanaydilek@gmail.com>
2023-04-02 16:48:20 +0300

    🤪

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm <em>Arda</em>. I'm a software enginer and a translator. You cannot contact me.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
 
 
